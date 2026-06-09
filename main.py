e29bd9f40bb59dab41f32e26527c87802b0607ba
Kauhe07 <kauhematos@gmail.com>
2026-06-09 16:14:22 -0300

    Modificado

---
+++ main.py
@@ -0,0 +1,8 @@
+for numero in range(10):
+    anterior = numero - 1
+    soma = numero + anterior
+
+    print(f"Número atual: {numero}")
+    print(f"Número anterior: {anterior}")
+    print(f"Soma: {soma}")
+    print("-" * 20)
